8ff75f73f6f306abceddeee5ea45038169df6f43
AusCyber <willp@outlook.com.au>
2021-06-24 17:30:31 +1000

    using env var

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
-    port: 9000,
+    port: process.env.PORT || 9000,
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
